326f13961796dc7a46267d4aa3adfabda95f7e07
Inaridiy <inainainariinainari@gmail.com>
2022-12-28 23:45:36 +0900

    feat: design

--- packages/interface/src/components/Layouts/HomeLayout.tsx
+++ packages/interface/src/components/Layouts/HomeLayout.tsx
@@ -1,7 +1,21 @@
+import { BsListUl, BsWallet } from "react-icons/bs";
+
 import { ThemeBox } from "../Elements";
 
 export const HomeLayout: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  return <ThemeBox className="min-h-[100dvh] bg-base-200">{children}</ThemeBox>;
+  return (
+    <ThemeBox className="min-h-[100dvh] bg-base-100">
+      <main>{children}</main>
+      <div className="fixed bottom-0 w-full flex justify-center p-2 bg-base-100 gap-6">
+        <button className="btn btn-square btn-ghost">
+          <BsListUl className="w-8 h-8" />
+        </button>
+        <button className="btn btn-square btn-ghost">
+          <BsWallet className="w-8 h-8" />
+        </button>
+      </div>
+    </ThemeBox>
+  );
 };
